0683111b84bb4f12ec2424b87615f4ed59fbe425
Wagner Souza <wagnersza@gmail.com>
2016-08-11 16:36:48 -0300

    add some hardening configs

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -1,4 +1,58 @@
-# Remove package setroubleshoot
-yum erase setrouleshoot
+# disable sending packages redirect
+echo "net.ipv4.conf.all.send_redirects=0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.send_redirects=0" >> /etc/sysctl.conf
 
-# 
+# disable ICMP redirect
+echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default. accept_redirects =0" >> /etc/sysctl.conf
+
+# enable logging of suspicious packages
+echo "net.ipv4.conf.all.log_martians=1" >> /etc/sysctl.conf
+echo "net.ipv4.conf.default.log_martians=1" >> /etc/sysctl.conf
+
+# disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+
+# keep all audit logs
+sed -i -e 's/max_log_file_action = ROTATE/max_log_file_action = keep_logs/g' /etc/audit/auditd.conf
+
+# Record events that modify date and time information
+cat >/etc/audit/audit.rules <<EOL
+-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
+-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
+-a always,exit -F arch=b64 -S clock_settime -k time-change
+-a always,exit -F arch=b32 -S clock_settime -k time-change
+-w /etc/localtime -p wa -k time-change
+EOL
+
+# Record events that modify user and group information
+cat >/etc/audit/audit.rules <<EOL
+-w /etc/group -p wa -k identity
+-w /etc/passwd -p wa -k identity
+-w /etc/gshadow -p wa -k identity
+-w /etc/shadow -p wa -k identity
+-w /etc/security/opasswd -p wa -k identity
+EOL
+
+# Record events that modify network enviroment
+cat >/etc/audit/audit.rules <<EOL
+-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale
+-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale
+-w /etc/issue -p wa -k system-locale
+-w /etc/issue.net -p wa -k system-locale
+-w /etc/hosts -p wa -k system-locale
+-w /etc/sysconfig/network -p wa -k system-locale
+EOL
+
+# Record events that modify the mandatory access control system
+cat >/etc/audit/audit.rules <<EOL
+-w /etc/selinux/ -p wa –k MAC-policy
+EOL
+
+# Record login and logout events
+cat >/etc/audit/audit.rules <<EOL
+-w /var/log/faillog -p wa -k logins
+-w /var/log/lastlog -p wa -k logins
+-w /var/log/tallylog -p wa -k logins
+EOL
+
