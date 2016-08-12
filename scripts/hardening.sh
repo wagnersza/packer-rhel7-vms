9b941ac2859658111513843a99fcd29a3d24b4a7
Wagner Souza <wagnersza@gmail.com>
2016-08-12 10:49:38 -0300

    more hardening and typo

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -1,61 +1,100 @@
 #!/usr/bin/env bash
 set -x
 
+# Network config 
+cat >> /etc/sysctl.conf <<EOL
+
 # disable sending packages redirect
-echo "net.ipv4.conf.all.send_redirects=0" >> /etc/sysctl.conf
-echo "net.ipv4.conf.default.send_redirects=0" >> /etc/sysctl.conf
+net.ipv4.conf.all.send_redirects=0
+net.ipv4.conf.default.send_redirects=0
 
 # disable ICMP redirect
-echo "net.ipv4.conf.all.accept_redirects=0" >> /etc/sysctl.conf
-echo "net.ipv4.conf.default. accept_redirects =0" >> /etc/sysctl.conf
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default. accept_redirects=0
 
 # enable logging of suspicious packages
-echo "net.ipv4.conf.all.log_martians=1" >> /etc/sysctl.conf
-echo "net.ipv4.conf.default.log_martians=1" >> /etc/sysctl.conf
+net.ipv4.conf.all.log_martians=1
+net.ipv4.conf.default.log_martians=1
 
 # disable IPv6
-echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
+net.ipv6.conf.all.disable_ipv6=1
+EOL
 
 # keep all audit logs
 sed -i -e 's/max_log_file_action = ROTATE/max_log_file_action = keep_logs/g' /etc/audit/auditd.conf
 
-# Record events that modify date and time information
+# Audit rules
 cat >> /etc/audit/rules.d/audit.rules <<EOL
+# Log events that modify date and time information
 -a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change
 -a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change
 -a always,exit -F arch=b64 -S clock_settime -k time-change
 -a always,exit -F arch=b32 -S clock_settime -k time-change
 -w /etc/localtime -p wa -k time-change
-EOL
 
-# Record events that modify user and group information
-cat >> /etc/audit/rules.d/audit.rules <<EOL
+# Log events that modify user and group information
 -w /etc/group -p wa -k identity
 -w /etc/passwd -p wa -k identity
 -w /etc/gshadow -p wa -k identity
 -w /etc/shadow -p wa -k identity
 -w /etc/security/opasswd -p wa -k identity
-EOL
 
-# Record events that modify network enviroment
-cat >> /etc/audit/rules.d/audit.rules <<EOL
+# Log events that modify network enviroment
 -a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale
 -a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale
 -w /etc/issue -p wa -k system-locale
 -w /etc/issue.net -p wa -k system-locale
 -w /etc/hosts -p wa -k system-locale
 -w /etc/sysconfig/network -p wa -k system-locale
-EOL
 
-# Record events that modify the mandatory access control system
-cat >> /etc/audit/rules.d/audit.rules <<EOL
+# Log events that modify the mandatory access control system
 -w /etc/selinux/ -p wa –k MAC-policy
-EOL
 
-# Record login and logout events
-cat >> /etc/audit/rules.d/audit.rules <<EOL
+# Log login and logout events
 -w /var/log/faillog -p wa -k logins
 -w /var/log/lastlog -p wa -k logins
 -w /var/log/tallylog -p wa -k logins
-EOL
 
+# Log session startup information
+-w /var/run/utmp -p wa -k session
+-w /var/log/wtmp -p wa -k session
+-w /var/log/btmp -p wa -k session
+
+# Log permission modification event of discretionary access control
+-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod
+-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod
+
+# Log unsuccessful attempts to access unauthorized files
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access
+-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access 
+
+# Log successful File System mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+
+# Log delete files events by users
+-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete
+-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete 
+
+# Log changes in system administration scope
+-w /etc/sudoers –p wa –k scope
+
+# Log system administrator actions
+-w /var/log/sudo.log –p wa –k actions
+
+# Log loading and unloading kernel modules
+-w /sbin/insmod -p x -k modules
+-w /sbin/rmmod -p x -k modules
+-w /sbin/modprobe -p x -k modules
+-a always,exit -F arch=b64 -S init_module -S delete_module -k modules
+
+# Makes immutable audit configuration
+-e 2
+
+EOL
