4a899da30f7fefc633c283f21efa0eaef6432e9d
Wagner Souza <wagnersza@gmail.com>
2016-08-12 18:20:12 -0300

    add hardening configs to ssh

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -2,5 +2,23 @@
 set -x
 
 echo "Modifying /etc/ssh/sshd_config..."
-echo "UseDNS no" >> /etc/ssh/sshd_config
-echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+
+sed -i -e 's/X11Forwarding yes/X11Forwarding no/g' /etc/ssh/sshd_config
+
+cat >> /etc/ssh/sshd_config <<EOL
+UseDNS no
+GSSAPIAuthentication no
+Protocol 2
+LogLevel INFO
+MaxAuthTries 4
+IgnoreRhosts yes
+HostbasedAuthentication no
+PermitRootLogin no
+PermitEmptyPasswords no
+PermitUserEnvironment no
+Ciphers aes128-ctr,aes192-ctr,aes256-ctr
+ClientAliveInterval 300
+ClientAliveCountMax 0
+Banner /etc/issue.net
+HostbasedAuthentication no
+EOL
